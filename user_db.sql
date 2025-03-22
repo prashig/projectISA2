6d774d9ea1d08e0aff39ec920447321a60ffb6db
AmnaMuzavor <amnamuzavor@gmail.com>
2025-03-22 20:27:35 +0530

    sql file
    the database file

--- user_db.sql
+++ user_db.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 22, 2025 at 03:05 PM
+-- Generation Time: Mar 22, 2025 at 03:55 PM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,41 +28,45 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `classes` (
-  `id` int(11) NOT NULL,
+  `c_id` int(11) NOT NULL,
   `className` varchar(255) NOT NULL,
   `classCode` varchar(10) NOT NULL,
-  `created_by` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_by` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `classes`
 --
 
-INSERT INTO `classes` (`id`, `className`, `classCode`, `created_by`, `created_at`) VALUES
-(1, 'php course', '946DD0', 2, '2025-03-21 08:31:59'),
-(3, 'english', '7C7785', 1, '2025-03-21 10:09:37'),
-(4, 'maths', 'F0EBEB', 2, '2025-03-22 05:22:42');
+INSERT INTO `classes` (`c_id`, `className`, `classCode`, `created_by`) VALUES
+(6, 'french', '1B766E', 3),
+(7, 'maths', 'C11DE8', 3),
+(8, 'science', '83911A', 3),
+(9, 'english1', 'C254BC', 4),
+(10, 'science', '5EFDAC', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `class_members`
+-- Table structure for table `notes`
 --
 
-CREATE TABLE `class_members` (
-  `id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `class_id` int(11) NOT NULL,
-  `joined_at` timestamp NOT NULL DEFAULT current_timestamp()
+CREATE TABLE `notes` (
+  `noteId` int(11) NOT NULL,
+  `noteTitle` varchar(255) NOT NULL,
+  `filePath` varchar(255) NOT NULL,
+  `uploadedBy` int(11) NOT NULL,
+  `classId` int(11) NOT NULL,
+  `uploadDate` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `class_members`
+-- Dumping data for table `notes`
 --
 
-INSERT INTO `class_members` (`id`, `user_id`, `class_id`, `joined_at`) VALUES
-(2, 1, 1, '2025-03-21 10:00:24');
+INSERT INTO `notes` (`noteId`, `noteTitle`, `filePath`, `uploadedBy`, `classId`, `uploadDate`) VALUES
+(4, 'unit 1', 'uploads/new22.pdf', 4, 9, '2025-03-22 11:24:58'),
+(5, 'test', 'uploads/new22.pdf', 4, 9, '2025-03-22 11:42:48');
 
 -- --------------------------------------------------------
 
@@ -71,10 +75,10 @@ INSERT INTO `class_members` (`id`, `user_id`, `class_id`, `joined_at`) VALUES
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `id` int(50) NOT NULL,
+  `name` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -82,8 +86,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
-(1, 'preet', 'preetgaude@gmail.com', '$2y$10$m09JsQ44j7PQkL8rrtkfb.w4qhQhubjDhxvQzfjPdJ9MpHsewCbiO'),
-(2, 'presha', 'bh@gmail', '$2y$10$/FX51amvympi9YuwwUPif.W3wsbks.ENYi9Ymoelsxrk/AbwC7WLK');
+(1, 'amna', 'amnamuzavor@gmail.com', '$2y$10$tjzvIEBu9hb/SH3g.BBvrOPaV5Z57a6X1i3VAEl9ns.F8N5/zVWgW'),
+(2, 'sam', 'sam@gmail.com', '$2y$10$76CyrpWK52xUp7cQO.Azs.NdTt2JaeTyGM6PFER4ZEFQf5b7DVwqu'),
+(3, 'sameena', 's@gmail.com', '$2y$10$2WWgCAJd7RpiomrWAClNK.Xd9Mt/JaB1P3DK6HuhrT7amlPUe//SC'),
+(4, 'amna', 'am@gmail.com', '$2y$10$BIh3/0QzEmT3GFh0bedtDuFrMpLYoXZWVilIYbYFMaeo3St3gbnKi');
 
 --
 -- Indexes for dumped tables
@@ -93,17 +99,17 @@ INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `classCode` (`classCode`),
+  ADD PRIMARY KEY (`c_id`),
+  ADD UNIQUE KEY `classcode` (`classCode`),
   ADD KEY `created_by` (`created_by`);
 
 --
--- Indexes for table `class_members`
+-- Indexes for table `notes`
 --
-ALTER TABLE `class_members`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `user_id` (`user_id`,`class_id`),
-  ADD KEY `class_id` (`class_id`);
+ALTER TABLE `notes`
+  ADD PRIMARY KEY (`noteId`),
+  ADD KEY `uploadedBy` (`uploadedBy`),
+  ADD KEY `classId` (`classId`);
 
 --
 -- Indexes for table `users`
@@ -120,19 +126,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `c_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `class_members`
+-- AUTO_INCREMENT for table `notes`
 --
-ALTER TABLE `class_members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `notes`
+  MODIFY `noteId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -142,14 +148,14 @@ ALTER TABLE `users`
 -- Constraints for table `classes`
 --
 ALTER TABLE `classes`
-  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`);
 
 --
--- Constraints for table `class_members`
+-- Constraints for table `notes`
 --
-ALTER TABLE `class_members`
-  ADD CONSTRAINT `class_members_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `class_members_ibfk_2` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE;
+ALTER TABLE `notes`
+  ADD CONSTRAINT `notes_ibfk_1` FOREIGN KEY (`uploadedBy`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `notes_ibfk_2` FOREIGN KEY (`classId`) REFERENCES `classes` (`c_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
